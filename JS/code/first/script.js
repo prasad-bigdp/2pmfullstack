1f68d6b175289bf89e1a00bb91a5fa013c955a05
prasad-bigdp <bigdp585@gmail.com>
2024-11-29 15:24:21 +0530

    js started

---
+++ JS/code/first/script.js
@@ -0,0 +1,13 @@
+/* 
+This is External
+JS- multi line
+Comment
+*/
+var a = 25;
+console.log("The value is " + (a + 1));
+/* try to take two variables and print its sum in console */
+var x = 10, y = 20;
+var c = x + y;
+console.log(a)
+alert("The sum is " + c);
+confirm("Are you sure?")
