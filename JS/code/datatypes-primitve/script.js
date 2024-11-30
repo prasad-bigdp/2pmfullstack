b261cfe7e268501978fa8fc62025fa7ffe70f742
prasad-bigdp <bigdp585@gmail.com>
2024-11-30 15:52:00 +0530

    js started

---
+++ JS/code/datatypes-primitve/script.js
@@ -0,0 +1,60 @@
+var a = 36,b=27;
+console.log(Math.sqrt(a), Math.cbrt(b));
+console.log(Math.abs(-10))
+console.log(Math.max(5, 6, 1, 5, 6, 9, 23))
+console.log(Math.min(5, 6))
+console.log(Math.floor(4.8))
+console.log(Math.ceil(4.1))
+console.log(Math.round(4.5))
+console.log(Math.pow(3,4),3**4)
+console.log(Math.round(Math.random() * 10000))
+/*write js code to print 4 digit <OTP></OTP>*/
+console.log(Math.floor(Math.random()))
+/* write a logic to print one dice for ludo game*/
+console.log(Math.ceil(Math.random() * 6))
+var num = 25.3157;
+console.log(num.toFixed(2))//25.32
+console.log(num.toPrecision('5'))//21.316
+var a=13
+console.log(a.toString(2))
+// converts to binary, 1101
+//convert any value to number by Number() or 'unary +'
+console.log(5 + Number('4'))
+console.log(5 - -'4')
+
+//strings
+//There is no difference between single quotation or double quotation
+var name = "Prasad"
+var n = 'prasad';
+console.log(typeof name, typeof n)
+var quote = "The gandhi said, 'Good night'"
+var msg= "The time is 6'o clock"
+//Strings can be represented by third ways also
+var str = `apple`;
+console.log(typeof str)
+//backticks are used to take dynamic value
+var cost = 25, quantity = 4;
+console.log("I bought " + quantity + " apples. Each cost ₹" + cost + " .so total is " + (cost * quantity))
+console.log(`I bought ${quantity} apples. Each cost ₹${cost}. so tatal is ${cost*quantity}`)
+// This way of writing dynamic strings is called Template literal
+
+/* write a code to find a+b whole sqaure formula result by inputting a,b values and print result using template literal */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
