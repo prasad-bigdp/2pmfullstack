552a6fb25709d62b80ecb1e0f71703f128cedfc2
prasad-bigdp <bigdp585@gmail.com>
2024-12-03 15:44:28 +0530

    apis

--- JS/code/datatypes-primitve/script.js
+++ JS/code/datatypes-primitve/script.js
@@ -1,44 +1,103 @@
-var a = 36,b=27;
-console.log(Math.sqrt(a), Math.cbrt(b));
-console.log(Math.abs(-10))
-console.log(Math.max(5, 6, 1, 5, 6, 9, 23))
-console.log(Math.min(5, 6))
-console.log(Math.floor(4.8))
-console.log(Math.ceil(4.1))
-console.log(Math.round(4.5))
-console.log(Math.pow(3,4),3**4)
-console.log(Math.round(Math.random() * 10000))
+// var a = 36,b=27;
+// console.log(Math.sqrt(a), Math.cbrt(b));
+// console.log(Math.abs(-10))
+// console.log(Math.max(5, 6, 1, 5, 6, 9, 23))
+// console.log(Math.min(5, 6))
+// console.log(Math.floor(4.8))
+// console.log(Math.ceil(4.1))
+// console.log(Math.round(4.5))
+// console.log(Math.pow(3,4),3**4)
+// console.log(Math.round(Math.random() * 10000))
 /*write js code to print 4 digit <OTP></OTP>*/
-console.log(Math.floor(Math.random()))
+// console.log(Math.floor(Math.random()))
 /* write a logic to print one dice for ludo game*/
-console.log(Math.ceil(Math.random() * 6))
-var num = 25.3157;
-console.log(num.toFixed(2))//25.32
-console.log(num.toPrecision('5'))//21.316
-var a=13
-console.log(a.toString(2))
+// console.log(Math.ceil(Math.random() * 6))
+// var num = 25.3157;
+// console.log(num.toFixed(2))//25.32
+// console.log(num.toPrecision('5'))//21.316
+// var a=13
+// console.log(a.toString(2))
 // converts to binary, 1101
 //convert any value to number by Number() or 'unary +'
-console.log(5 + Number('4'))
-console.log(5 - -'4')
+// console.log(5 + Number('4'))
+// console.log(5 - -'4')
 
 //strings
 //There is no difference between single quotation or double quotation
-var name = "Prasad"
-var n = 'prasad';
-console.log(typeof name, typeof n)
-var quote = "The gandhi said, 'Good night'"
-var msg= "The time is 6'o clock"
+// var name = "Prasad"
+// var n = 'prasad';
+// console.log(typeof name, typeof n)
+// var quote = "The gandhi said, 'Good night'"
+// var msg= "The time is 6'o clock"
 //Strings can be represented by third ways also
-var str = `apple`;
-console.log(typeof str)
+// var str = `apple`;
+// console.log(typeof str)
 //backticks are used to take dynamic value
-var cost = 25, quantity = 4;
-console.log("I bought " + quantity + " apples. Each cost ₹" + cost + " .so total is " + (cost * quantity))
-console.log(`I bought ${quantity} apples. Each cost ₹${cost}. so tatal is ${cost*quantity}`)
+// var cost = 25, quantity = 4;
+// console.log("I bought " + quantity + " apples. Each cost ₹" + cost + " .so total is " + (cost * quantity))
+// console.log(`I bought ${quantity} apples. Each cost ₹${cost}. so tatal is ${cost*quantity}`)
 // This way of writing dynamic strings is called Template literal
 
 /* write a code to find a+b whole sqaure formula result by inputting a,b values and print result using template literal */
+
+
+
+
+
+var str = "I love javascript   "
+console.log(str[0]) //I
+console.log(str.length) //17
+//length returns total number of characters
+console.log(str.charAt(15)) //p
+console.log(str.indexOf('a'))//8
+//indeOf returns indexof first occurance
+console.log(str.lastIndexOf('a')); //10
+console.log(str.startsWith("I l")) //True
+console.log(str.endsWith("script")) //True
+console.log(str.trim()) //Trim will removes spaces from last and first of the given string
+console.log(str.replace('a',"k"))
+// it replaces for only one time
+console.log(str.toUpperCase())
+//It converts given string into uppercase
+console.log(str.toLowerCase())
+// it converts given string into lowercase
+str="I Love JS"
+console.log(str.split(''))
+//split is used to split the characters based on delimiter
+console.log(str.slice(2,5))
+//slice is used to get sub string of specific index to specific index
+console.log(str.slice(-1))// last value
+
+
+
+
+// check from the given whether they are belongs to agarwals or not
+var n = "pranav agarwal"
+console.log(n.endsWith("agarwal"))
+// check whether a person's middle is mayen or not
+var n1 = "John Mayen patrick"
+console.log(n1.split(' ')[1] == "Mayen")
+//how to check an input value empty or not
+//inp.trim()==""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ JS/code/workingOfJs/script.js
@@ -0,0 +1,5 @@
+console.log(a,b)
+var a = 25;
+var x = null;
+console.log(a);
+var b = 30;
