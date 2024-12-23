87dfae8f008c4b84746f49564eee59f3497421ba
prasad-bigdp <bigdp585@gmail.com>
2024-12-23 12:55:17 +0530

    update

--- JS/code/DOM3/script.js
+++ JS/code/DOM3/script.js
@@ -10,14 +10,30 @@ var c = 0;
 
 var mname = document.getElementById('mname');
 var murl = document.getElementById('url');
-var getMovie = function ()
+// var getMovie = function ()
+// {
+//     var mdiv = document.createElement('div');
+//     var mimg = document.createElement('img');
+//     mimg.src = murl.value;
+//     mimg.alt = mname.value;
+//     var mtitle = document.createElement('h2')
+//     mtitle.textContent = mname.value;
+//     mdiv.append(mimg, mtitle)
+//     mainDiv.appendChild(mdiv)
+// }
+
+var changeColor = function (event)
 {
-    var mdiv = document.createElement('div');
-    var mimg = document.createElement('img');
-    mimg.src = murl.value;
-    mimg.alt = mname.value;
-    var mtitle = document.createElement('h2')
-    mtitle.textContent = mname.value;
-    mdiv.append(mimg, mtitle)
-    mainDiv.appendChild(mdiv)
+    console.log(event.key)
+    // if (event.key == 'Enter')
+    // {
+      const rdcolor = "#" + Math.round(Math.random() * 10000000).toString(16)
+			document.body.style.backgroundColor = rdcolor  
+    // }
+    
 }
+document.addEventListener('scroll', function (e)
+{
+  console.log(e)
+})
+document.body.addEventListener("keydown", changeColor)
---
+++ JS/code/form-validation/script.js
@@ -0,0 +1,53 @@
+// var,let,const
+// console.log(b) // cannot access let,const before declaration
+// var a = 20;
+// var a = 15; // redeclare is possible using var
+// a = 30; // reassign is possible using var
+// let b = 20;
+//let b = 15; //let and const cannot declare
+// b = 30;// reassign is possible using let
+// const c = 10;
+// c = 20;// const values cannot re assign
+//var is a function scoped variable and let,const are block scoped variables
+// function sum ()
+// {
+//     if (true)
+//     {
+//         let x = 25;
+//     }
+//     console.log(x)
+// }
+// sum()
+
+
+const nameInput= document.getElementById('name').value
+const emailInput = document.getElementById("email").value
+const passwordInput = document.getElementById("pwd").value
+const passwordInput2 = document.getElementById("pwd2").value
+
+
+document.getElementById('form').addEventListener('submit', function (e)
+{
+    e.preventDefault();
+    console.log(e);
+    if (nameInput.trim().length < 6)
+    {
+        alert("name should be minimum 6 characters")
+    }
+    if (!(emailInput.match("/^S+@S+.S{3}+$/")))
+    {
+        document.getElementById('emailError').textContent="Please enter valid email"
+    }
+    if (passwordInput != passwordInput2)
+    {
+        alert("Passwords are not matching")
+    }
+    
+})
+function revealPassword() {
+	if (passwordInput2.type == "text") {
+		passwordInput2.type = "password"
+	} else {
+		passwordInput2.type = "text"
+	}
+}
